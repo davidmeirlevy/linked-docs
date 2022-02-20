3c056cd11c0dd2493c0bc07420800ba180a68e02
David Meir-Levy <davidmeirlevy@gmail.com>
2022-02-20 16:20:56 +0200

    upgrade

--- packages/linked-docs/vite.config.ts
+++ packages/linked-docs/vite.config.ts
@@ -4,7 +4,6 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
 	plugins: [vue()],
 	build: {
-		target: 'esnext',
 		lib: {
 			entry: './src/index.ts',
 			name: 'LinkedItems',
